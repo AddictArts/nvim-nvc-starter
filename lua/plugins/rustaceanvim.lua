54996be2a740adead8c2317c693585edf37e2415
John Pywtorak <jpywtora@gmail.com>
2024-03-19 22:34:02 -0700

    Added inlay hints 0.9 and prior

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,29 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^4", -- Recommended
-	ft = { "rust" },
+  "mrcjkb/rustaceanvim",
+  version = "^4", -- Recommended
+  dependencies = {
+    {
+      "lvimuser/lsp-inlayhints.nvim",
+      opts = {},
+    },
+  },
+  ft = { "rust" },
+  config = function()
+    vim.g.rustaceanvim = {
+      inlay_hints = {
+        highlight = "NonText",
+      },
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+        },
+      },
+      server = {
+        on_attach = function(client, bufnr)
+          local il_hints = require "lsp-inlayhints"
+          il_hints.on_attach(client, bufnr)
+        end,
+      },
+    }
+  end,
 }
