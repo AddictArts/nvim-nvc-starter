ed8da2080eb70ebf15947c0b815058e1229e35c2
John Pywtorak <jpywtora@gmail.com>
2024-03-22 14:41:42 -0700

    CodeLLDB integration Windows specific right now

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -9,6 +9,17 @@ return {
   },
   ft = { "rust" },
   config = function()
+    -- Update this path
+    local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
+    local codelldb_path = extension_path .. "adapter/codelldb"
+    local liblldb_path = extension_path .. "lldb/lib/liblldb"
+
+    -- The path is different on Windows
+    codelldb_path = extension_path .. "adapter\\codelldb.exe"
+    liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+
+    local cfg = require "rustaceanvim.config"
+
     vim.g.rustaceanvim = {
       inlay_hints = {
         highlight = "NonText",
@@ -23,6 +34,9 @@ return {
           local il_hints = require "lsp-inlayhints"
           il_hints.on_attach(client, bufnr)
         end,
+      },
+      dap = {
+        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
       },
     }
   end,
