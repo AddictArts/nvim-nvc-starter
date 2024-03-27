880cd179b566abba3465b5985a140c2ad61d433f
john.pywtorak <john.pywtorak@sri.com>
2024-03-27 12:05:54 -0700

    Added lspconfig to plugins init adding dockerls

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,11 +24,18 @@ return {
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
-
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
       git = { enable = true },
     },
+  },
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
   },
 }
